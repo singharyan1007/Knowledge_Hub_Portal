67fa8dfc8c69585a6c3cb34b992b5081f632013a
Aryan <singh.aryan837@gmail.com>
2024-08-14 09:41:29 +0530

    Initialised

---
+++ KnowledgeHubPortal.Domain/Repository/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KnowledgeHubPortal.Domain.Entities;
+
+namespace KnowledgeHubPortal.Domain.Repository
+{
+    public  interface ICategoryRepository
+    {
+        void Create(Category category);
+    }
+}
